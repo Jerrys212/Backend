399e2b8702e37424469b31182f5ea878db9db2c8
ggarcia <ggarcia@petco.com.mx>
2023-07-30 16:14:45 -0600

    a

---
+++ scripts/addMesas.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const mesas = [
+  { mesa: "1", cantidadPersonas: "2", disponible: true },
+  { mesa: "2", cantidadPersonas: "4", disponible: true },
+  { mesa: "3", cantidadPersonas: "6", disponible: true },
+  { mesa: "4", cantidadPersonas: "2", disponible: true },
+  { mesa: "5", cantidadPersonas: "4", disponible: true },
+  { mesa: "6", cantidadPersonas: "6", disponible: true },
+  { mesa: "7", cantidadPersonas: "2", disponible: true },
+  { mesa: "8", cantidadPersonas: "4", disponible: true },
+];
+
+const url = "https://devdesignhub.com/pos/nuevaMesa";
+
+mesas.forEach((mesa) => {
+  axios
+    .post(url, mesa)
+    .then((res) => console.log(res.data))
+    .catch((err) => console.log(err));
+});
